cebae12b0ee44bd467b83b12432e7884f53015ab
Prince Patel <pspatel53@myseneca.ca>
2025-02-05 23:47:11 -0500

    Lab-4 Done

---
+++ lab4d.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Strings 1
+
+str1 = 'Hello World!!'
+str2 = 'Seneca College'
+
+num1 = 1500
+num2 = 1.50
+
+def first_five(x):
+    return x[0:5]
+
+def last_seven(x):
+    return x[-7:]    
+
+def middle_number(x):
+    a = str(x)
+    b = a[1:3]
+    return b
+
+def first_three_last_three(x, y):
+    a = x[0:3]
+    b = y[-3:]
+    return a + b
+
+if __name__ == '__main__':
+
+    print(first_five(str1))
+
+    print(first_five(str2))
+
+    print(last_seven(str1))
+
+    print(last_seven(str2))
+
+    print(middle_number(num1))
+
+    print(middle_number(num2))
+
+    print(first_three_last_three(str1, str2))
+
+    print(first_three_last_three(str2, str1))
